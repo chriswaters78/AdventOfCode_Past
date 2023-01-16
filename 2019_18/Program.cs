568ea6140469d4892aa2210f2d4e8ce2014c987b
Christian <cwaters@truenorthit.co.uk>
2023-01-16 11:53:33 +0000

    only caching junctions

--- 2019_18/Program.cs
+++ 2019_18/Program.cs
@@ -4,7 +4,7 @@ namespace _2019_18
 {
     internal class Program
     {
-        record struct State((uint keys, int keyCount) s, (int r, int c) pos);
+        record struct State(uint keys, (int r, int c) pos);
         
         static Dictionary<State, int> cache = new Dictionary<State, int>();
         static Dictionary<(int r, int c), char> maze;
@@ -13,75 +13,79 @@ namespace _2019_18
         static uint SetBit(int bit, uint state) => state |= 1U << bit;
         static uint ClearBit(int bit, uint state) => state &= ~(1U << bit);
 
+        static uint finalKeys = 0;
+
         static void Main(string[] args)
         {
             Stopwatch watch = new Stopwatch();
             watch.Start();
-            maze = File.ReadAllLines("part2.txt").SelectMany((line, r) => line.Select((ch, c) => (r, c, ch)))
+            maze = File.ReadAllLines("input.txt").SelectMany((line, r) => line.Select((ch, c) => (r, c, ch)))
                 .Where(tp => tp.ch != '#')
                 .ToDictionary(tp => (tp.r, tp.c), tp => tp.ch);
 
             var start = maze.First(kvp => kvp.Value == '@').Key;
             var keyCount = maze.Count(kvp => Char.IsLower(kvp.Value));
             var allKeys = maze.Values.Where(ch => Char.IsLower(ch)).OrderBy(ch => ch).ToArray();
-            uint finalKeys = 0;
             foreach (var ch in allKeys)
             {
                 finalKeys = SetBit(ch - 'a', finalKeys);
             }
 
-            solve(new State((0, 0), start));
+            solve(new State(0, start));
 
-            var best = cache.Where(kvp => kvp.Key.s.keys == finalKeys).Min(kvp => kvp.Value);
+            var best = cache.Where(kvp => kvp.Key.keys == finalKeys).Min(kvp => kvp.Value);
             Console.WriteLine($"Part 1: {best} in {watch.ElapsedMilliseconds}ms");
+            Console.WriteLine($"Cache entries: {cache.Count}");
 
         }
 
+        static (int or, int oc)[] offsets = new [] { (0, 1), (0, -1), (1, 0), (-1, 0) };
+
         static void solve(State state)
         {
-            cache.Add(state, 0);
-            var queue = new Queue<(State state, (int r, int c) lastPosition)>();
-            queue.Enqueue((state, (-1,-1)));
+            var queue = new Queue<(State state, (int r, int c) lastPosition, int time)>();
+            queue.Enqueue((state, (-1,-1), 0));
 
             while (queue.Count > 0)
             {
-                (state, var lastPosition) = queue.Dequeue();
-                var t = cache[state];
+                (state, var lastPosition, var time) = queue.Dequeue();
 
+                var neighbours = offsets.Select(offset => (state.pos.r + offset.or, state.pos.c + offset.oc)).Where(edge => maze.ContainsKey(edge)).ToArray();
+                var isJunction = neighbours.Length > 2;
                 //we can move to any of the 4 spaces around
-                foreach (var edge in new[] { (0, 1), (0, -1), (1, 0), (-1, 0) }.Select(tp => (state.pos.r + tp.Item1, state.pos.c + tp.Item2)))
+                foreach (var edge in neighbours)
                 {
                     if (lastPosition == edge)
                     {
                         //never backtrack, clear lastPosition if we find a key
                         continue;
                     }
-                    if (!maze.ContainsKey(edge))
-                    {
-                        continue;
-                    }
-                    if (Char.IsUpper(maze[edge]) && !IsBitSet(Char.ToLower(maze[edge]) - 'a', state.s.keys))
+                    if (Char.IsUpper(maze[edge]) && !IsBitSet(Char.ToLower(maze[edge]) - 'a', state.keys))
                     {
                         //locked door
                         continue;
                     }
 
-                    var isKey = Char.IsLower(maze[edge]) && !IsBitSet(maze[edge] - 'a', state.s.keys);
+                    var isNewKey = Char.IsLower(maze[edge]) && !IsBitSet(maze[edge] - 'a', state.keys);
                     State newState = state with
                     {
                         pos = edge,
-                        s = isKey
-                            ? (SetBit(maze[edge] - 'a', state.s.keys), state.s.keyCount + 1)
-                            : state.s
+                        keys = isNewKey
+                            ? SetBit(maze[edge] - 'a', state.keys)
+                            : state.keys
                     };
 
-                    if (cache.ContainsKey(newState) && cache[newState] <= t + 1)
+                    if (isJunction || isNewKey)
                     {
-                        //been there before but better
-                        continue;
+                        if (cache.ContainsKey(newState) && cache[newState] <= time + 1)
+                        {
+                            //been there before but better
+                            continue;
+                        }
+                        cache[newState] = time + 1;
                     }
-                    cache[newState] = t + 1;
-                    queue.Enqueue((newState, isKey ? (-1,-1) : state.pos));
+
+                    queue.Enqueue((newState, isNewKey ? (-1,-1) : state.pos, time + 1));
                 }
             }
         }
