1047e8d98fbb11688725e030825e9ecce039793a
Christian <cwaters@truenorthit.co.uk>
2023-10-06 17:10:20 +0100

    started 2018-04

---
+++ 2018_04/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int? currentGuard = null;
+DateTime? currentTime = null;
+DateTime? fellAsleep = null;
+bool isSleeping = false;
+Dictionary<int,int> guardSleeping = new Dictionary<int, int>();
+foreach (var line in File.ReadAllLines("test.txt"))
+{
+    var sp = line.Split("] ");
+    currentTime = DateTime.Parse(sp[0].Trim('['));
+    if (sp[1].StartsWith("Guard #"))
+    {
+        currentGuard = int.Parse(sp[1].Split(" ")[1].Trim('#'));
+    }
+    if (sp[1].StartsWith("falls"))
+    {
+        isSleeping = true;
+        fellAsleep = currentTime;
+    }
+    if (sp[1].StartsWith("wakes"))
+    {
+        isSleeping = false;
+        if (!guardSleeping.ContainsKey(currentGuard.Value))
+        {
+            guardSleeping.Add(currentGuard.Value, 0);
+        }
+        guardSleeping[currentGuard.Value] += (currentTime.Value - fellAsleep.Value).Minutes;
+        fellAsleep = null;
+    }
+}
+var part1 = guardSleeping.OrderByDescending(kv => kv.Value).First();
