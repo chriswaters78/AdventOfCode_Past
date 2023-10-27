c4e22e9cdade9bb8a4c881fc2f3a7f94bfa7ec9e
Christian <cwaters@truenorthit.co.uk>
2023-10-27 15:57:25 +0100

    day 4 done

--- 2018_04/Program.cs
+++ 2018_04/Program.cs
@@ -4,8 +4,9 @@ int? currentGuard = null;
 DateTime? currentTime = null;
 DateTime? fellAsleep = null;
 bool isSleeping = false;
-Dictionary<int,int> guardSleeping = new Dictionary<int, int>();
-foreach (var line in File.ReadAllLines("test.txt"))
+var guardSleeping = new Dictionary<int, int>();
+var minutesAsleep = new Dictionary<int, List<int>>();
+foreach (var line in File.ReadAllLines("input.txt").OrderBy(l => l))
 {
     var sp = line.Split("] ");
     currentTime = DateTime.Parse(sp[0].Trim('['));
@@ -26,7 +27,26 @@ foreach (var line in File.ReadAllLines("test.txt"))
             guardSleeping.Add(currentGuard.Value, 0);
         }
         guardSleeping[currentGuard.Value] += (currentTime.Value - fellAsleep.Value).Minutes;
+        for (var minute = fellAsleep; minute < currentTime; minute = minute.Value.AddMinutes(1))
+        {
+            if (!minutesAsleep.ContainsKey(currentGuard.Value))
+            {
+                minutesAsleep.Add(currentGuard.Value, new List<int>());
+            }
+            minutesAsleep[currentGuard.Value].Add(minute.Value.Minute);
+        }
         fellAsleep = null;
     }
 }
-var part1 = guardSleeping.OrderByDescending(kv => kv.Value).First();
+var guardSleepsMost = guardSleeping.OrderByDescending(kv => kv.Value).First().Key;
+var minuteSleepsMost = minutesAsleep[guardSleepsMost].GroupBy(i => i).OrderByDescending(g => g.Count()).First().Key;
+var part1 = guardSleepsMost * minuteSleepsMost;
+
+Console.WriteLine($"Part 1: {part1}");
+
+var guardSleepsMost2 = 
+        minutesAsleep.Select(kv => new { Guard = kv.Key, Minute = kv.Value.GroupBy(i => i).OrderByDescending(g => g.Count()).First() })
+        .OrderByDescending(kv => kv.Minute.Count()).First();
+
+var part2 = guardSleepsMost2.Guard * guardSleepsMost2.Minute.Key;
+Console.WriteLine($"Part 2: {part2}");
