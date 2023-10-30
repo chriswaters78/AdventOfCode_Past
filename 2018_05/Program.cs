f132d32faca6bc1f5ba43470d8181a844c727a0b
Christian Waters <cwaters@truenorthit.co.uk>
2023-10-30 16:14:54 +0000

    part 5 - day 1

---
+++ 2018_05/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+var input = new LinkedList<char>(File.ReadAllText("test.txt"));
+
+var current = input.First;
+while (current.Next != null)
+{
+    if ((char.IsLower(current.Value) && char.ToUpper(current.Value) == current.Next.Value)
+        || (char.IsUpper(current.Value) && char.ToLower(current.Value) == current.Next.Value))
+    {
+        input.Remove(current.Next);
+        input.Remove(current);
+        current = input.First;
+    }
+    else
+    {
+        current = current.Next;
+    }
+}
+
+Console.WriteLine($"Part 1: {input.Count}");
