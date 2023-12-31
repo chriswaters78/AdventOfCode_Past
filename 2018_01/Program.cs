d51aa3e45b0f387eec9430ac386479d5b52e199e
Christian Waters <cwaters@truenorthit.co.uk>
2023-12-31 22:56:37 +0000

    Auto stash before checking out "origin/main"

---
+++ 2018_01/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine(File.ReadAllLines("input.txt").Sum(int.Parse));
+
+var input = File.ReadAllLines("input.txt").Select(int.Parse).ToArray();
+var seen = new HashSet<int>();
+int f = 0, i = 0;
+do
+{
+    f += input[i];
+    i = (i + 1) % input.Length;
+}
+while (seen.Add(f));
+
+Console.WriteLine(f);
