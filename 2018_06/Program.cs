6d0ca58d0abc83a26da7502fed231adf098cecef
Christian Waters <cwaters@truenorthit.co.uk>
2024-01-01 14:57:51 +0000

    part 6 stub

---
+++ 2018_06/Program.cs
@@ -0,0 +1,7 @@
+﻿//if a point is within a a diagonal quadrant from the point:
+//            B   #E
+//             # #
+//              P
+//then B would block P upwards, E wouldn't as there will always be a point directly above P that is closer to P than E
+
+//so for all our points find any that are blocked in all directions
