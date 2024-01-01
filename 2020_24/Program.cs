5dc6a03bae81c77867c404140c7c40d3b0d617cb
Christian Waters <cwaters@truenorthit.co.uk>
2024-01-01 14:06:42 +0000

    2020 day 24

--- 2020_24/Program.cs
+++ 2020_24/Program.cs
@@ -1,154 +1,70 @@
-﻿var input = File.ReadAllLines("input.txt").Select(line => line.ToArray()).ToArray();
+﻿using System.Numerics;
 
-uint state = 0;
-for (int r = 0; r < 5; r++)
+var offsets = new List<(string prefix, Complex offset)>()
 {
-    for (int c = 0; c < 5; c++)
+    ("ne", new Complex(0, 1)),
+    ("e", new Complex(1, 0)),
+    ("se", new Complex(1, -1)),
+    ("sw", new Complex(0, -1)),
+    ("w", new Complex(-1, 0)),
+    ("nw", new Complex(-1, 1)),
+};
+
+var file = File.ReadAllLines("input.txt");
+var instructions = file.Select(_ => new List<Complex>()).ToList();
+foreach ((var line, var index) in file.Select((str,i) => (str,i)))
+{
+    var curr = line;
+    while (curr.Length > 0)
     {
-        if (input[r][c] == '#')
-        {
-            state = SetBit(r * 5 + c, state);
-        }
+        var offset = offsets.Where(os => curr.StartsWith(os.prefix)).Single();
+        instructions[index].Add(offset.offset);
+        curr = curr[offset.prefix.Length..];
     }
 }
 
-var offsets =
-    (from r in Enumerable.Range(0, 5)
-     from c in Enumerable.Range(0, 5)
-     select (r, c))
-    .ToDictionary(key => key.r * 5 + key.c,
-    key => new[] { (key.r - 1, key.c), (key.r + 1, key.c), (key.r, key.c - 1), (key.r, key.c + 1) }
-        .Where(key => key.Item1 >= 0 && key.Item1 < 5 && key.Item2 >= 0 && key.Item2 < 5
-                && !(key.Item1 == 2 && key.Item2 == 2))
-        .Select(key => (key.Item1 * 5 + key.Item2, 0)).ToList());
-
-//for recursive, these are modified
-offsets[0].Add((11, -1));
-offsets[0].Add((7, -1));
-offsets[1].Add((7, -1));
-offsets[2].Add((7, -1));
-offsets[3].Add((7, -1));
-offsets[4].Add((7, -1));
-offsets[4].Add((13, -1));
-
-offsets[5].Add((11, -1));
-offsets[10].Add((11, -1));
-offsets[15].Add((11, -1));
-offsets[9].Add((13, -1));
-offsets[14].Add((13, -1));
-offsets[19].Add((13, -1));
-
-offsets[20].Add((11, -1));
-offsets[20].Add((17, -1));
-offsets[21].Add((17, -1));
-offsets[22].Add((17, -1));
-offsets[23].Add((17, -1));
-offsets[24].Add((17, -1));
-offsets[24].Add((13, -1));
-
-offsets[7].AddRange(new[] { (0, 1), (1, 1), (2, 1), (3, 1), (4, 1) });
-offsets[17].AddRange(new[] { (20, 1), (21, 1), (22, 1), (23, 1), (24, 1) });
-
-offsets[11].AddRange(new[] { (0, 1), (5, 1), (10, 1), (15, 1), (20, 1) });
-offsets[13].AddRange(new[] { (4, 1), (9, 1), (14, 1), (19, 1), (24, 1) });
-
-var states = new Dictionary<int, uint>();
-states[0] = state;
-
-for (int steps = 0; steps < 200; steps++)
+var flipped = new HashSet<Complex>();
+foreach (var instruction in instructions)
 {
-    //Console.WriteLine($"Step {steps}");
-    //print(states);
-    //seen.Add(state);
-    var minLevel = states.Keys.Min();
-    var maxLevel = states.Keys.Max();
+    var position = new Complex(0, 0);
+    foreach (var move in instruction)
+        position += move;
 
-    states[minLevel - 1] = 0;
-    states[maxLevel + 1] = 0;
-    states[minLevel - 2] = 0;
-    states[maxLevel + 2] = 0;
-    var newStates = states.ToDictionary(kvp => kvp.Key, _ => (uint) 0);
-    for (int level = minLevel - 1; level <= maxLevel + 1; level++)
+    if (flipped.Contains(position))
+        flipped.Remove(position);
+    else
+        flipped.Add(position);
+}
+
+
+Console.WriteLine($"Part1: {flipped.Count}");
+
+for (int days = 1; days <= 100; days++)
+{
+    //need to check one bigger than the max
+    (int minX, int maxX) = ((int)flipped.Min(c => c.Real) - 1, (int)flipped.Max(c => c.Real) + 1);
+    (int minY, int maxY) = ((int)flipped.Min(c => c.Imaginary) - 1, (int)flipped.Max(c => c.Imaginary) + 1);
+
+    var newFlipped = new HashSet<Complex>();
+
+    for (int x = minX; x <= maxX; x++)
     {
-        for (int bit = 0; bit < 25; bit++)
+        for (int y = maxY; y >= minY; y--)
         {
-            if (bit == 12)
+            var curr = new Complex(x, y);
+            var blackCount = offsets.Where(os => flipped.Contains(curr + os.offset)).Count();
+            if (flipped.Contains(curr))
             {
-                continue;
+                if (blackCount != 0 && blackCount <= 2)
+                    newFlipped.Add(curr);
             }
-
-            var count = offsets[bit].Count(os => IsBitSet(os.Item1, states[level + os.Item2]));
-            if (IsBitSet(bit, states[level]))
+            else if (blackCount == 2)
             {
-                if (count == 1)
-                {
-                    newStates[level] = SetBit(bit, newStates[level]);
-                }
-            }
-            else
-            {
-                if (count == 1 || count == 2)
-                {
-                    newStates[level] = SetBit(bit, newStates[level]);
-                }
+                newFlipped.Add(curr);
             }
         }
     }
-    states = newStates;
-}
-//while (!seen.Contains(state));
-
-Console.WriteLine($"Part 1: {state}");
-
-var part2 = 0;
-foreach (var level in states.Keys)
-{
-    for (int bit = 0; bit < 25; bit++)
-    {
-        if (bit == 12)
-        {
-            continue;
-        }
-        if (IsBitSet(bit, states[level]))
-        {
-            part2++;
-        }
-    }
+    flipped = newFlipped;
 }
 
-//Console.WriteLine($"Final state:");
-//print(states);
-Console.WriteLine($"Part 2: {part2}");
-
-static bool IsBitSet(int bit, uint state) => (state & (1U << bit)) != 0;
-static uint SetBit(int bit, uint state) => state |= 1U << bit;
-static uint ClearBit(int bit, uint state) => state &= ~(1U << bit);
-
-static void print(Dictionary<int, uint> states)
-{
-    foreach (var level in states.Keys.OrderBy(key => key))
-    {
-        if (states[level] == 0)
-        {
-            continue;
-        }
-        Console.WriteLine();
-        Console.WriteLine($"Level {level}:");
-        for (int r = 0; r < 5; r++)
-        {
-            for (int c = 0; c < 5; c++)
-            {
-                var bit = r * 5 + c;
-                if (bit == 12)
-                {
-                    Console.Write('?');
-                }
-                else
-                {
-                    Console.Write(IsBitSet(bit, states[level]) ? '#' : '.');
-                }
-            }
-            Console.WriteLine();
-        }
-    }
-}
+Console.WriteLine($"Part2: {flipped.Count}");
