92f2acc2bd6fac49874eef66065269e5f216c46d
Christian <cwaters@truenorthit.co.uk>
2024-01-12 12:08:06 +0000

    2018 day 9 and day 10

---
+++ 2018_09/Program.cs
@@ -0,0 +1,37 @@
+﻿var arr = File.ReadAllText("input.txt").Split(" ").ToArray();
+(var players, var lastMarble) = (int.Parse(arr[0]), int.Parse(arr[6]));
+
+var part1 = solve(players, lastMarble);
+Console.WriteLine($"Part1: {part1}");
+var part2 = solve(players, lastMarble * 100); 
+Console.WriteLine($"Part2: {part2}");
+
+long solve(int players, int lastMarble)
+{
+    var played = new LinkedList<int>();
+    played.AddLast(0);
+    var current = played.First;
+    var player = 0;
+    var playerScores = new long[players];
+    for (int marble = 1; marble <= lastMarble; marble++)
+    {
+        if (marble % 23 == 0)
+        {
+            playerScores[player] += marble;
+            for (int i = 0; i < 7; i++)
+            {
+                current = current?.Previous ?? played.Last;
+            }
+            playerScores[player] += current.Value;
+            current = current?.Next ?? played.First;
+            played.Remove(current.Previous);
+        }
+        else
+        {
+            current = played.AddAfter(current?.Next ?? played.First, marble);
+        }
+        player = (player + 1) % players;
+    }
+
+    return playerScores.Max();
+}
---
+++ 2018_10/Program.cs
@@ -0,0 +1,29 @@
+﻿//position=<-20620, -41485> velocity=< 2,  4>
+var points = File.ReadAllLines("test.txt").Select(line => line.Replace("position=<", "").Trim().Replace(",", "").Replace("> velocity=<", " ").Replace("  ", " ").Replace(">", "").Split(" ").Select(int.Parse).ToArray()).ToArray();
+
+var T = (points[0][0] - points[1][0]) / (points[1][2] - points[0][2]);
+
+for (int t = T - 2; t <= T + 2; t++)
+{
+    HashSet<(int r, int c)> pointsAtt = points.Select(p => (p[1] + t * p[3], p[0] + t * p[2])).ToHashSet();
+    (var minC, var maxC) = (pointsAtt.Min(a => a.c), pointsAtt.Max(a => a.c));
+    (var minR, var maxR) = (pointsAtt.Min(a => a.r), pointsAtt.Max(a => a.r));
+
+    Console.WriteLine($"After {t}:");
+
+    for (int r = minR; r <= maxR; r++)
+    {
+        for (int c = minC; c <= maxC; c++)
+        {
+            if (pointsAtt.Contains((r, c)))
+            {
+                Console.Write('#');
+            }
+            else
+            {
+                Console.Write(' ');
+            }
+        }
+        Console.WriteLine();
+    }
+}
